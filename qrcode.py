58af362e4d59ba5389d535a3607c0183bca3b4d3
Vallepu Vamsi Krishna <53989958+vamsivallepu@users.noreply.github.com>
2020-02-12 05:36:39 +0530

    Update qrcode.py

--- qrcode.py
+++ qrcode.py
@@ -1,5 +1,9 @@
 #python code to create a qrcode
+
 import pyqrcode #imports pyqrcode module into the code
-url = pyqrcode.create("www.instagram.com") #assaigning a website to our qr code
+
+url = pyqrcode.create("www.instagram.com") #creating we code and assaigning a website to our qr code
+
 url.svg('uca-url.svg',scale=10) #creates output file(qrcode) as a .svg file
+
 print(url.terminal(quiet_zone=1)) #rendering qrcode to terminal (able to be scanned by scanners
