bcb933a4a577a04691f5859a523e97cc99f79972
vamsivallepu2 <82360630+vamsivallepu2@users.noreply.github.com>
2021-04-12 12:01:24 +0530

    Add files via upload

--- qrcode.py
+++ qrcode.py
@@ -7,3 +7,5 @@ url = pyqrcode.create("www.instagram.com") #creating we code and assaigning a we
 url.svg('uca-url.svg',scale=10) #creates output file(qrcode) as a .svg file
 
 print(url.terminal(quiet_zone=1)) #rendering qrcode to terminal (able to be scanned by scanners
+
+#testing
